c545f3c1037708ab5cd13f20ee965f32b0e40fe4
neko113 <alstn113@gmail.com>
2022-11-10 19:05:07 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { MainLayout } from './components/MainLayout';
+
+const App = () => {
+  return (
+    <MainLayout>
+      <Header />
+      <Footer />
+    </MainLayout>
+  );
+};
+
+export default App;
