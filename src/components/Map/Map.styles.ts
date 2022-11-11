b58dc22f5693dcd7b78b5d0713b0534f775dbaf9
neko113 <alstn113@gmail.com>
2022-11-11 13:04:58 +0900

    fix: map container

--- src/components/Map/Map.styles.ts
+++ src/components/Map/Map.styles.ts
@@ -1,10 +1,24 @@
 import styled from '@emotion/styled';
 
 export const Root = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 50px auto 150px;
+`;
+
+export const Container = styled.div`
+  border: 10px solid #fff;
   height: 500px;
   width: 80%;
-  margin: 1rem auto;
   @media screen and (max-width: 580px) {
     width: 100%;
+    height: 300px;
   }
+`;
+
+export const Title = styled.h1`
+  font-size: 20px;
+  color: #fff;
 `;
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -5,14 +5,20 @@ import { Map as KakaoMap, MapMarker } from 'react-kakao-maps-sdk';
 const Location = () => {
   return (
     <S.Root>
-      <KakaoMap
-        center={{ lat: 35.13277, lng: 129.106902 }}
-        style={{ width: '100%', height: '360px' }}
-        level={4}
-        draggable={false}
-      >
-        <MapMarker position={{ lat: 35.13277, lng: 129.106902 }} />
-      </KakaoMap>
+      <S.Title>장소: 부경대학교 창의관 303호</S.Title>
+      <S.Container>
+        <KakaoMap
+          center={{ lat: 35.13277, lng: 129.106902 }}
+          style={{
+            objectFit: 'cover',
+            width: '100%',
+            height: '100%',
+          }}
+          level={4}
+        >
+          <MapMarker position={{ lat: 35.13277, lng: 129.106902 }} />
+        </KakaoMap>
+      </S.Container>
     </S.Root>
   );
 };
