bfe314a2e969aa4924fcead1f78313632b37d87d
neko113 <alstn113@gmail.com>
2022-11-11 11:10:23 +0900

    feat: add schedule icons

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -6,6 +6,54 @@ import 'react-vertical-timeline-component/style.min.css';
 import * as S from './Schedule.styles';
 
 const Schedule = () => {
+  const bag = (
+    <span className="vertical-timeline-element-icon bounce-in">
+      <svg
+        className="MuiSvgIcon-root"
+        focusable="false"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path
+          d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z"
+          fill="currentColor"
+        ></path>
+      </svg>
+    </span>
+  );
+
+  const star = (
+    <span className="vertical-timeline-element-icon bounce-in">
+      <svg
+        className="MuiSvgIcon-root"
+        focusable="false"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path
+          d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+          fill="currentColor"
+        ></path>
+      </svg>
+    </span>
+  );
+
+  const bachelorCap = (
+    <span className="vertical-timeline-element-icon bounce-in">
+      <svg
+        className="MuiSvgIcon-root"
+        focusable="false"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path
+          d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"
+          fill="currentColor"
+        ></path>
+      </svg>
+    </span>
+  );
+
   return (
     <S.Root>
       <VerticalTimeline lineColor="rgba(33, 150, 243, 0.5)">
@@ -18,6 +66,7 @@ const Schedule = () => {
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
           date="2011 - present"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={bag}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
           <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -35,7 +84,7 @@ const Schedule = () => {
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
           date="2011 - present"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<div />}
+          icon={bag}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
           <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -53,7 +102,25 @@ const Schedule = () => {
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
           date="2011 - present"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<div />}
+          icon={bag}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: 'linear-gradient(to bottom, rgb(233,33,90), #f07497)',
+            color: '#fff',
+          }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(233, 33, 90)' }}
+          date="2011 - present"
+          iconStyle={{ background: 'rgb(233, 33, 90)', color: '#fff' }}
+          icon={bachelorCap}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
           <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -71,7 +138,7 @@ const Schedule = () => {
           contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
           date="2011 - present"
           iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          icon={<div />}
+          icon={bag}
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
           <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -82,7 +149,7 @@ const Schedule = () => {
         </VerticalTimelineElement>
         <VerticalTimelineElement
           iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-          icon={<div />}
+          icon={star}
         />
       </VerticalTimeline>
     </S.Root>
